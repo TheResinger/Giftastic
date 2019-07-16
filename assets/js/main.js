9c9bef563d082c884a9d0acad8d4f43412174675
TheResinger <morrowbenjamin42@gmail.com>
2019-07-15 23:31:53 -0400

    Finished ajax call and population, TODO: style sheet

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var animalArray = ["Bird", "Cat", "Dog", "Ferret", ];
     function updateList()
     {
+        $("#animalButtons").empty();
         for(var i = 0; i < animalArray.length; i++)
         {
             $("#animalButtons").append($("<button>",{"type":"button","id":animalArray[i],"text":animalArray[i],"class":"animalButton"}));
@@ -10,14 +11,16 @@ $(document).ready(function(){
     }
     $("#addAnimal").on("click", function(event) {
         event.preventDefault();
-       console.log($("#animal").val());
+        console.log($("#animal").val());
+        animalArray.push($("#animal").val());
+        updateList();
     });
    
     $(document).on("click",".animalButton", function(){
         console.log($(this).attr("id"));
         var queryAnimal = $(this).attr("id");
-        var queryURL =  "https://api.giphy.com/v1/gifs/search?q=" + queryAnimal + "&api_key=2II6IkwKhCCu8L1lvmmQUB53I3TFGZpI&limit=10"
-
+        var queryURL =  "https://api.giphy.com/v1/gifs/search?q=" + queryAnimal + "&api_key=2II6IkwKhCCu8L1lvmmQUB53I3TFGZpI&limit=50"
+        $("#gifGrid").empty();
         $.ajax({
             url : queryURL,
             method : 'GET',
@@ -26,11 +29,25 @@ $(document).ready(function(){
             var results = response.data;
             for(var i = 0; i < results.length; i++)
             {
-                $("#gifGrid").prepend($("<div>",{"class" : "gif col-md-4","id" : queryAnimal + i}));
+                $("#gifGrid").prepend($("<div>",{"class" : "col-md-4","id" : queryAnimal + i}));
                 $("#" + queryAnimal + i).append($("<p>",{"text":"Rating : "+ results[i].rating}));
-                $("#" + queryAnimal + i).append($("<img>",{"src": results[i].images.fixed_height.url}));
+                $("#" + queryAnimal + i).append($("<img>",{"src": results[i].images.fixed_height_still.url,"data-still":results[i].images.fixed_height_still.url,"data-animated": results[i].images.fixed_height.url,"data-state": "still","class" : "gif"}));
             }
         });
+    });
+    $(document).on("click", ".gif", function(){
+        var state = $(this).attr("data-state");
+        console.log(state);
+        if(state === "still")
+        {
+            $(this).attr("src", $(this).attr("data-animated"));
+            $(this).attr("data-state", "animate");
+        }
+        else
+        {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
     });
     updateList();
 });
